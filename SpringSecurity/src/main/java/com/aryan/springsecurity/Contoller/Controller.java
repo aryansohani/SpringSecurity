8b3b451625247efce696732fad5cdddde602676f
Aryan Sohani <aryan.112123@gmail.com>
2026-05-06 23:13:34 +0530

    Learnt about default security form

---
+++ SpringSecurity/src/main/java/com/aryan/springsecurity/Contoller/Controller.java
@@ -0,0 +1,17 @@
+package com.aryan.springsecurity.Contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String greet()
+    {
+        return "hello world";
+    }
+
+
+
+}
