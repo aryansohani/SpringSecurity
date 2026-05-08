7b3ee35098487b266df78ccfed92c770f666abe4
Aryan Sohani <aryan.112123@gmail.com>
2026-05-08 19:10:55 +0530

    SessionID

--- SpringSecurity/src/main/java/com/aryan/springsecurity/Contoller/Controller.java
+++ SpringSecurity/src/main/java/com/aryan/springsecurity/Contoller/Controller.java
@@ -1,5 +1,6 @@
 package com.aryan.springsecurity.Contoller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/hello")
-    public String greet()
+    public String greet(HttpServletRequest request)
     {
-        return "hello world";
+        return "hello world"+ request.getSession().getId();
+    }
+    @GetMapping("/About")
+    public String about(HttpServletRequest request)
+    {
+        return "hello welcome aryan"+ request.getSession().getId();
     }
 
 
