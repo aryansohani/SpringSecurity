33044721d86d391d5329f6be5a2d68de60b9224d
Aryan Sohani <aryan.112123@gmail.com>
2026-05-16 22:44:37 +0530

    save local project

---
+++ SpringCSRFtoken/src/main/java/com/aryan/springcsrftoken/SpringCsrFtokenApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.springcsrftoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCsrFtokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCsrFtokenApplication.class, args);
+    }
+
+}
---
+++ SpringCSRFtoken/src/test/java/com/aryan/springcsrftoken/SpringCsrFtokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.springcsrftoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCsrFtokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
