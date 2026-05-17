9dc0d50ee828c91a293a95bedc61aebdeb5da020
Aryan Sohani <aryan.112123@gmail.com>
2026-05-17 22:40:21 +0530

    Doing self based configuation using @enablewebsecurity and securityFilterChain

---
+++ SpringSecurityCofigurations/src/main/java/com/aryan/springsecuritycofigurations/SpringSecurityCofigurationsApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.springsecuritycofigurations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCofigurationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityCofigurationsApplication.class, args);
+    }
+
+}
---
+++ SpringSecurityCofigurations/src/test/java/com/aryan/springsecuritycofigurations/SpringSecurityCofigurationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.springsecuritycofigurations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCofigurationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
